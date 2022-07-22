d06fa38f81fe0ce377e958d98059d2fcb498875b
Daniel Yang <danyang0724@gmail.com>
2022-07-22 12:26:33 -0400

    feat: added validators for all cases

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+  validate :true_title
+
+  def true_title
+    if self.title == "True Facts"
+      errors.add(:title, "You are not allowed to have True Facts as a title")
+    end
+  end
+
 end
